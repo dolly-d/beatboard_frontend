3c5ef45ae4b4d63bd59f25897382240d34c952d4
dolly-d <dolly.desir@gmail.com>
2020-08-17 17:29:58 -0400

    some sound playing!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", (event) => { 
 
+const piano = document.getElementById('piano')
+// console.log(piano)
+
+
+piano.addEventListener('click', (e) => {
+    const eventTarget = e.target.innerText
+    const playKeyA = document.getElementById(`A${eventTarget}`)
+    playKeyA.play()
+    const playKeyB = document.getElementById(`B${eventTarget}`)
+    playKeyB.play()
+    console.log(playKeyB)
+})
+
+// function playKey(key){
+
+// }
 
 
 
@@ -7,10 +23,4 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
 
-
-
-
-
-
-    
 })
